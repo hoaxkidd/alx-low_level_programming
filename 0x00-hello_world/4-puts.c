4939a9099590b4fc27e982caceee63f7d26c46fc
Temidayo O. Abdul Azeez <hoaxkidd@gmail.com>
2023-03-16 16:21:44 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
-#inclde <stdio.h>
-/** 
- * main - A program that prints  a line with puts function
-n
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function
  *Return: 0 (Success)
  */
 int main(void)
