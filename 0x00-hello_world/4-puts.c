5585d82a43d45eefe84719064647386e0b3588f8
Temidayo O. Abdul Azeez <hoaxkidd@gmail.com>
2023-03-16 16:05:29 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#inclde <stdio.h>
+/** 
+ * main - A program that prints  a line with puts function
+n
+ *Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
